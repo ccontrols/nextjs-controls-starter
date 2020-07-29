ed76fbf72252a9b02edb0efb4611b632a46deb66
atanasster <atanasster@gmail.com>
2020-07-29 11:28:29 -0400

    initial check-in

---
+++ .config/runtime.ts
@@ -0,0 +1,7 @@
+export default {
+  siteTitle: `awLib`,
+  siteHeadline: `Awesome components only`,
+  siteUrl: `https://component-controls-gatsby.netlify.app`,
+  siteDescription: `Some description meta.`,
+  author: 'my name'
+};
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { GetStaticProps } from 'next';
+import { DocType, defDocType } from '@component-controls/core';
+import { DocPage } from '@component-controls/app';
+import { Layout, store, getIndexPage } from '@component-controls/nextjs-plugin';
+
+interface PageListProps {
+  type: DocType;
+  docId?: string;
+}
+
+const HomePage: FC<PageListProps> = ({ type = defDocType, docId }) => {
+  return (
+    <Layout docId={docId}>
+      <DocPage type={type} />
+    </Layout>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const homePage = getIndexPage(store);
+  const { docId = null, type = null } = homePage;
+  return { props: { docId, type } };
+};
+
+export default HomePage;
