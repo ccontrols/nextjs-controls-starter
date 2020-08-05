bc49320ad0ba3d9325c6ccfdb3f3757d80519fc8
atanasster <atanasster@gmail.com>
2020-08-05 10:06:25 +0300

    bump dependency

--- .config/runtime.ts
+++ .config/runtime.ts
@@ -1,6 +1,5 @@
 export default {
   siteTitle: `awLib`,
-  siteHeadline: `Awesome components only`,
   siteUrl: `https://component-controls-gatsby.netlify.app`,
   siteDescription: `Some description meta.`,
   author: 'my name'
