1c24070ce22a1ce6e39c503d18eb06bf5acbfee4
atanasster <atanasster@gmail.com>
2020-12-24 00:30:04 -0500

    upgrade packages

--- .config/buildtime.js
+++ .config/buildtime.js
@@ -1,10 +1,18 @@
+const { defaultBuildConfig } = require("@component-controls/core");
+
 module.exports = {
-  stories: [
-    '../src/docs/*.@(mdx|tsx)',
-  ],
+  stories: ["../src/docs/*.@(mdx|tsx)"],
   pages: {
     story: {
-      tabs: [{ route: 'page' }, { route: 'test' }],
+      tabs: [
+        ...defaultBuildConfig.pages.story.tabs,
+        {
+          route: "test",
+          title: "Testing",
+          template: "@component-controls/pages/TestingPage",
+        },
+      ],
     },
-  }
+  },
+  siteUrl: `https://nextjs-controls-starter.netlify.app`,
 };
--- .config/runtime.tsx
+++ .config/runtime.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { RunConfiguration, defaultRunConfig } from "@component-controls/core";
-import { TestingPage } from "./TestingPage";
 
 
 const config: RunConfiguration = {
@@ -8,14 +7,6 @@ const config: RunConfiguration = {
   siteUrl: `https://nextjs-controls-starter.netlify.app`,
   description: `Some description meta.`,
   author: 'my name',
-  pages: {
-    story: {
-      tabs: [
-        ...defaultRunConfig.pages.story.tabs,
-        { title: 'Testing', render: () => <TestingPage /> },
-      ],
-    },
-  }  
 };
 
 export default config;
