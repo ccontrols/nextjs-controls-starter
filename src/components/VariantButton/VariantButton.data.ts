8a51c2449777509b7f31b92695a60650aecc68d8
atanasster <atanasster@gmail.com>
2021-05-09 07:04:00 +0300

    update packages

---
+++ src/components/VariantButton/VariantButton.data.ts
@@ -0,0 +1,24 @@
+export default {
+  overview: {
+    "0": {
+      text: "Penelope O'Reilly",
+      icon: "Boyd Strosin"
+    },
+    "1": {
+      text: "Miss Bettye Gerhold",
+      icon: "Lyric Leannon"
+    },
+    "2": {
+      text: "Hilma Rohan",
+      icon: "Cathy Price"
+    },
+    "3": {
+      text: "Marty Mann",
+      icon: "Harold Davis"
+    },
+    "4": {
+      text: "Priscilla Jones",
+      icon: "Shannon Jones"
+    }
+  }
+};
