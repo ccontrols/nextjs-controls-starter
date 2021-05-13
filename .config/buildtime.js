b9d4820fbfe97598686300f3ae02bfb4edabe50e
atanasster <atanasster@gmail.com>
2021-05-13 12:37:57 +0300

    cleanup config

--- .config/buildtime.js
+++ .config/buildtime.js
@@ -2,13 +2,5 @@ const { defaultBuildConfig } = require("@component-controls/core");
 
 module.exports = {
   stories: ["../src/docs/*.mdx", "../src/components/**/*.docs.tsx"],
-  pages: {
-    story: {
-      tabs: {
-        ...defaultBuildConfig.pages.story.tabs,
-        test: "@component-controls/pages/TestingPage",
-      },
-    },
-  },
   siteUrl: `https://nextjs-controls-starter.netlify.app`,
 };
